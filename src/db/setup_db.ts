5873ca546f97b88fb202d8de7b85dec65a5e3491
Rishab Gupta <rishabsgupta@gmail.com>
2017-03-02 23:18:08 -0500

    code cleanup

--- src/db/setup_db.ts
+++ src/db/setup_db.ts
@@ -1,15 +1,18 @@
 /**
  * A standalone script for configuring the local couchbase database.
  */
+
 // Read .env file
 import * as dotenv from 'dotenv';
 dotenv.config();
-import {AsyncBucket, AsyncClusterManager} from 'couchbase'
+
+import {AsyncClusterManager} from 'couchbase'
 import {couchbaseClient} from './couchbaseClient';
 import * as Promise from 'bluebird';
 
 // The couchbase buckets that we're setting up.
 let bucket_names = ['users', 'classes'];
+// How we want to setup these buckets.
 let bucket_options = {
   bucketType: 'couchbase',
   ramQuotaMB: 100,
@@ -17,18 +20,25 @@ let bucket_options = {
   saslPassword: 'BUCKET_PASSWORD' in process.env ? process.env['BUCKET_PASSWORD'] : '',
 };
 
-let clusterManager = couchbaseClient.openAsyncClusterManager()
-clusterManager.listBucketsAsync().then((rows) => {
-  // Delete existing buckets.
-  console.log('Deleting existing buckets...');
-  return Promise.each(rows, (row) => {
-    return clusterManager.removeBucketAsync(row.name);
-  });
-}).then(() => {
-  console.log('Adding clicker buckets...');
-  Promise.each(bucket_names, (name) => {
-    return clusterManager.createBucketAsync(name, bucket_options);
-  });
-}).then(() => {
-  console.log('Setup completed, buckets successfully created');
-}).done();
+let main = (): void => {
+  let clusterManager = couchbaseClient.openAsyncClusterManager()
+  clusterManager.listBucketsAsync().then((rows) => {
+    // Delete existing buckets.
+    console.log('Deleting existing buckets...');
+    return Promise.each(rows, (row) => {
+      return clusterManager.removeBucketAsync(row.name);
+    });
+  }).then(() => {
+    console.log('Adding clicker buckets...');
+    Promise.each(bucket_names, (name) => {
+      return clusterManager.createBucketAsync(name, bucket_options);
+    });
+  }).then(() => {
+    console.log('Setup completed, buckets successfully created');
+  }).done();
+}
+
+// Runs the main function only on invocation, not import.
+if (require.main === module) {
+    main();
+}
