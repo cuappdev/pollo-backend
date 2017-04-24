72201dbd63cd0db47f9224dd3d49e91e8ae54ee2
Daniel Li <dl743@cornell.edu>
2017-04-24 12:19:52 -0400

    Add webpack dev server

--- app/App.js
+++ app/App.js
@@ -4,4 +4,4 @@ import { Router, browserHistory } from 'react-router';
 
 import routes from './routes';
 
-ReactDOM.render(<Router history={history} routes={routes} />, document.getElementById('root'));
+ReactDOM.render(<Router history={browserHistory} routes={routes} />, document.getElementById('root'));
