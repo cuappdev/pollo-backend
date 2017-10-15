c0d05e66258b02a8f34e62294969fa436f9fda58
Kevin <mr.kev@me.com>
2017-10-14 22:02:05 -0400

    GET /organizations/

---
+++ src/routers/GetOrganizationsRouter.js
@@ -0,0 +1,40 @@
+// @flow
+import { Request } from 'express';
+import AppDevEdgeRouter from '../utils/AppDevEdgeRouter';
+import constants from '../utils/constants';
+import OrganizationsRepo from '../repos/OrganizationsRepo'
+
+type APIOrganization = {
+  id: string,
+  name: string,
+}
+
+class GetOrganizations extends AppDevEdgeRouter<APIOrganization> {
+
+  constructor() {
+    super(constants.REQUEST_TYPES.GET);
+  }
+
+  getPath(): string {
+    return '/organizations/';
+  }
+
+  async contentArray(req, pageInfo, error) {
+
+    const orgs = await OrganizationsRepo
+      .paginateOrganization(pageInfo.cursor || 0, pageInfo.count)
+
+    return orgs
+      // filters falsy entries out
+      .filter(Boolean)
+      .map(org => ({
+        node: {
+          id: org.id,
+          name: org.name,
+        },
+        cursor: org.createdAt.valueOf(),
+      }));
+  }
+}
+
+export default new GetOrganizations().router;
