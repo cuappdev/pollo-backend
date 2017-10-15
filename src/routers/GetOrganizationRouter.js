758dd6aa444e25c4488eefbaec522d5fd5903012
Kevin <mr.kev@me.com>
2017-10-14 23:59:38 -0400

    fetch an org

---
+++ src/routers/GetOrganizationRouter.js
@@ -0,0 +1,24 @@
+// @flow
+import { Request } from 'express';
+import AppDevNodeRouter from '../utils/AppDevNodeRouter';
+import constants from '../utils/constants';
+import OrganizationsRepo from '../repos/OrganizationsRepo'
+
+import type { APIOrganization } from './APITypes'
+
+class GetOrganization extends AppDevNodeRouter<APIOrganization> {
+
+  getPath(): string {
+    return '/organizations/:id/';
+  }
+
+  async fetchWithId(id) {
+    const org = await OrganizationsRepo.getOrgById(id);
+    return org && {
+      id: org.id,
+      name: org.name,
+    }
+  }
+}
+
+export default new GetOrganization().router;
