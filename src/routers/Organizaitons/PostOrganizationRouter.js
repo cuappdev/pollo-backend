70e42107f8021966c14a0ceb9b83ee8283d32069
Megan Le <meganle7@gmail.com>
2017-10-29 22:00:19 -0400

    Create lecture router

--- src/routers/Organizaitons/PostOrganizationRouter.js
+++ src/routers/Organizaitons/PostOrganizationRouter.js
@@ -3,23 +3,23 @@ import { Request } from 'express';
 import AppDevRouter from '../../utils/AppDevRouter';
 import OrganizationsRepo from '../../repos/OrganizationsRepo';
 
-class GetUsersRouter extends AppDevRouter {
-  constructor() {
+class PostOrganizationRouter extends AppDevRouter {
+  constructor () {
     super('POST');
   }
 
-  getPath(): string {
+  getPath (): string {
     return '/organizations/';
   }
 
-  async content(req: Request) {
-    const name = req.body.name
-    if (!name) throw new Error('Name missing')
-    const org = await OrganizationsRepo.createOrganization(name)
+  async content (req: Request) {
+    const name = req.body.name;
+    if (!name) throw new Error('Name missing');
+    const org = await OrganizationsRepo.createOrganization(name);
     return {
-      node: org,
-    }
+      node: org
+    };
   }
 }
 
-export default new GetUsersRouter().router;
+export default new PostOrganizationRouter().router;
