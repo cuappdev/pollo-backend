f9ad23f3ad46142ace97e12a50a1884e78f60a0e
Megan Le <meganle7@gmail.com>
2017-11-14 10:47:03 -0500

    Put and delete routers for organizations

---
+++ src/routers/Organizaitons/DeleteOrganizationRouter.js
@@ -0,0 +1,23 @@
+// @flow
+import { Request } from 'express';
+import AppDevRouter from '../../utils/AppDevRouter';
+import OrganizationsRepo from '../../repos/OrganizationsRepo';
+import constants from '../../utils/constants';
+
+class DeleteOrganizationRouter extends AppDevRouter {
+  constructor () {
+    super(constants.REQUEST_TYPES.DELETE);
+  }
+
+  getPath (): string {
+    return '/organizations/:id/';
+  }
+
+  async content (req: Request) {
+    const orgId = req.params.id;
+    await OrganizationsRepo.deleteOrgById(orgId);
+    return null;
+  }
+}
+
+export default new DeleteOrganizationRouter().router;
