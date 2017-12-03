2630622b41b0f89dc06fa1f327a1f2b4ab88c750
Megan Le <meganle7@gmail.com>
2017-12-03 18:59:11 -0500

    fixed param

--- src/routers/Lectures/GetLiveLecturesRouter.js
+++ src/routers/Lectures/GetLiveLecturesRouter.js
@@ -3,6 +3,7 @@ import { Request } from 'express';
 import AppDevRouter from '../../utils/AppDevRouter';
 import constants from '../../utils/constants';
 import UsersRepo from '../../repos/UsersRepo';
+import LecturesRepo from '../../repos/LecturesRepo';
 import LectureManager from '../../LectureManager';
 
 class GetLiveLecturesRouter extends AppDevRouter {
@@ -11,12 +12,15 @@ class GetLiveLecturesRouter extends AppDevRouter {
   }
 
   getPath (): string {
-    return '/lectures/live/get/';
+    return '/users/:userId/lectures/';
   }
 
   async content (req: Request) {
-    const userId = req.body.userId;
+    const userId = req.params.userId;
+    // const live = req.query.live;
+
     if (!userId) throw new Error('User id is missing!');
+
     const courses = await UsersRepo.getAssocCoursesByUserId(userId, 'student');
     const lectures = await LectureManager.liveLectures(courses);
     return lectures
