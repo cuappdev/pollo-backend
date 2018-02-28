c87ba0c68a50b508598dea3b670da9942e01a532
Jehron Petty <jehronpetty@dhcp-mcfad-5128.eduroam.cornell.edu>
2018-02-27 19:16:47 -0500

    Added get, updated, and delete methods to the Question repo

--- test/question.test.js
+++ test/question.test.js
@@ -21,6 +21,23 @@ test('Create Question', async () => {
   expect(question.results).toEqual({});
 });
 
+test('Get Question', async () => {
+  const question = await QuestionsRepo.getQuestionById(id);
+  expect(question.text).toBe('Question');
+  expect(question.poll.id).toBe(poll.id);
+  expect(question.results).toEqual({});
+});
+
+test('Update Question', async () => {
+  const question = await QuestionsRepo.updateQuestionById(id, 'New Question');
+  expect(question.text)
+});
+
+test('Delete Question', async () => {
+  await QuestionsRepo.deleteQuestionById(id);
+  expect(await QuestionsRepo.getQuestionById(id)).not.toBeDefined();
+});
+
 // Teardown
 afterAll(async () => {
   await QuestionsRepo.deleteQuestionById(poll.id);
