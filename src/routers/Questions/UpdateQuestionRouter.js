7f0bd4155cadc89b2f7e264fe121dd6facecbb56
Jehron Petty <jehronpetty@cornell.edu>
2018-03-03 18:21:34 -0500

    Added 'UpdateQuestion' Router file with my janky dev env

---
+++ src/routers/Questions/UpdateQuestionRouter.js
@@ -0,0 +1,43 @@
+// @flow
+import { Request } from 'express';
+import AppDevRouter from '../../utils/AppDevRouter';
+import QuestionsRepo from '../../repos/QuestionsRepo';
+import constants from '../../utils/constants';
+
+import type { APIQuestion } from '../APITypes';
+
+class UpdateQuestionRouter extends AppDevRouter<Object> {
+  constructor () {
+    super(constants.REQUEST_TYPES.PUT);
+  }
+
+  getPath (): string {
+    return '/questions/:id/';
+  }
+
+  async content (req: Request): Promise<{ node: APIQuestion }> {
+    const questionId = req.params.id;
+    var text = req.body.text;
+    var results = req.body.results;
+
+    const prevQuestion = await QuestionsRepo.getQuestionById(questionId);
+    if (!prevQuestion) throw new Error(`Poll with id ${questionId} was not found!`);
+
+    if (!text) text = prevQuestion.text;
+    if (!results) throw new Error(`No fields specified to update.`);
+
+    const question = await QuestionsRepo.updateQuestionById(questionId, text, results);
+    if (!question) throw new Error(`Poll with id ${questionId} was not found!`);
+
+    return {
+      node : {
+        id: question.id,
+        text: question.text,
+        results: question.results
+      }
+    };
+
+  }
+}
+
+export default new UpdateQuestionRouter().router;
