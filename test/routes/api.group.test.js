dcc0b9cc9c78c62096a8231914f99e3e0d5ca9c2
Megan Le <meganle7@gmail.com>
2018-04-10 22:57:11 -0400

    Basic group route tests

--- test/routes/api.group.test.js
+++ test/routes/api.group.test.js
@@ -4,13 +4,13 @@ import UserSessionsRepo from '../../src/repos/UserSessionsRepo';
 const request = require('request-promise-native');
 const { get, post, del, put } = require('./lib');
 
-// Sessions
+// Groups
 // Must be running server to test
 
-const opts = {name: 'Test session', code: 'ABC123'};
-const opts2 = {name: 'New session'};
+const opts = {name: 'Test group', code: 'ABC123'};
+const opts2 = {name: 'New group'};
 const googleId = 'usertest';
-var token, session, sessionres, userId;
+var token, session, userId, groupRes;
 
 beforeAll(async () => {
   await dbConnection().catch(function (e) {
@@ -24,48 +24,48 @@ beforeAll(async () => {
   token = session.sessionToken;
 });
 
-test('create session', async () => {
-  const result = await request(post('/sessions/', opts, token));
-  sessionres = JSON.parse(result);
-  expect(sessionres.success).toBeTruthy();
+test('create group without session or initial members', async () => {
+  const result = await request(post('/groups/', opts, token));
+  groupRes = JSON.parse(result);
+  expect(groupRes.success).toBeTruthy();
 });
 
-test('get single session', async () => {
-  const getstr = await request(get(`/sessions/${sessionres.data.node.id}`, token));
+test('get single group', async () => {
+  const getstr = await request(get(`/groups/${groupRes.data.node.id}`, token));
   const getres = JSON.parse(getstr);
   expect(getres.success).toBeTruthy();
-  expect(sessionres).toMatchObject(getres);
+  expect(groupRes).toMatchObject(getres);
 });
 
-test('update session', async () => {
+test('update group', async () => {
   const getstr =
-    await request(put(`/sessions/${sessionres.data.node.id}`, opts2, token));
+    await request(put(`/groups/${groupRes.data.node.id}`, opts2, token));
   const getres = JSON.parse(getstr);
   expect(getres.success).toBeTruthy();
-  expect(getres.data.node.name).toBe('New session');
+  expect(getres.data.node.name).toBe('New group');
 });
 
 test('update session with invalid token', async () => {
   const getstr =
-    await request(put(`/sessions/${sessionres.data.node.id}`, opts2, 'invalid'));
+    await request(put(`/groups/${groupRes.data.node.id}`, opts2, 'invalid'));
   const getres = JSON.parse(getstr);
   expect(getres.success).toBeFalsy();
 });
 
 test('delete session with invalid token', async () => {
   const result =
-    await request(del(`/sessions/${sessionres.data.node.id}`, 'invalid'));
+    await request(del(`/groups/${groupRes.data.node.id}`, 'invalid'));
   expect(JSON.parse(result).success).toBeFalsy();
 });
 
 test('delete session', async () => {
   const result =
-    await request(del(`/sessions/${sessionres.data.node.id}`, token));
+    await request(del(`/groups/${groupRes.data.node.id}`, token));
   expect(JSON.parse(result).success).toBeTruthy();
 });
 
 afterAll(async () => {
   await UsersRepo.deleteUserById(userId);
   await UserSessionsRepo.deleteSession(session.id);
-  console.log('Passed all session route tests');
+  console.log('Passed all group route tests');
 });
