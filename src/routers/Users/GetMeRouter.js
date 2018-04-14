d98aaa176a0105867c915ec30a81c53cad7230ce
Megan Le <meganle7@gmail.com>
2018-04-14 17:40:49 -0400

    GetMeRouter

---
+++ src/routers/Users/GetMeRouter.js
@@ -0,0 +1,25 @@
+// @flow
+import { Request } from 'express';
+import AppDevRouter from '../../utils/AppDevRouter';
+import constants from '../../utils/constants';
+import type { APIUser } from '../APITypes';
+
+class GetMeRouter extends AppDevRouter<APIUser> {
+  constructor () {
+    super(constants.REQUEST_TYPES.GET);
+  }
+
+  getPath (): string {
+    return '/users/';
+  }
+
+  async content (req: Request) {
+    return req.user && {
+      id: req.user.id,
+      name: req.user.firstName + ' ' + req.user.lastName,
+      netId: req.user.netId
+    };
+  }
+}
+
+export default new GetMeRouter().router;
