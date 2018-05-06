25771d1f5f4c5f73dc9c4da7ac66ead33932a6fc
Jehron Petty <jehronpetty@cornell.edu>
2018-05-05 22:24:44 -0400

    tested draft routes

--- test/routes/api.draft.test.js
+++ test/routes/api.draft.test.js
@@ -15,17 +15,78 @@ beforeAll(async () => {
   userId = user.id;
   const session = await UserSessionsRepo.createOrUpdateSession(user, null, null);
   userToken = session.sessionToken;
+});
 
-  // Create a session
-  const opts = {
+test('Create a draft', async () => {
+  const body = {
     text: 'Test draft',
     options: ['yes', 'no']
   };
-  const result = await request(post('/sessions/', opts, userToken));
-  draft1 = result.data.node;
+  const getstr = await request(post('/drafts/', body, userToken));
+  const getres = getstr;
+  draft1 = getres.data.node;
+  expect(getres.success).toBeTruthy();
+  expect(draft1).toMatchObject(body);
+});
+
+test('Get drafts (Authorized)', async () => {
+  const getstr = await request(get('/drafts/', userToken));
+  const getres = getstr;
+  const edges = getres.data.edges;
+  expect(getres.success).toBeTruthy();
+  expect(edges.length).toBe(1);
+  expect(edges[0].node).toMatchObject(draft1);
+});
+
+test('Get drafts (Unauthorized)', async () => {
+  const getstr = await request(get('/drafts/', 'blah'));
+  const getres = getstr;
+  expect(getres.success).toBeFalsy();
+});
+
+test('Update a draft', async () => {
+  const body = {
+    text: 'Test draft updated'
+  }
+  const getstr = await request(put(`/drafts/${draft1.id}`, body, userToken));
+  const getres = getstr;
+  const node = getres.data.node;
+  expect(getres.success).toBeTruthy();
+  expect(node.text).toBe(body.text);
+  expect(node.options).toMatchObject(draft1.options);
+  expect(node.id).toBe(draft1.id);
+  draft1 = node;
+});
+
+test('Create another draft', async () => {
+  const body = {
+    text: 'Another One ... DJ Khaled',
+    options: ['yes']
+  };
+  const getstr = await request(post('/drafts/', body, userToken));
+  const getres = getstr;
+  draft2 = getres.data.node;
+  expect(getres.success).toBeTruthy();
+  expect(draft2).toMatchObject(body);
+});
+
+test('Get updated list of drafts (Authorized)', async () => {
+  const getstr = await request(get('/drafts/', userToken));
+  const getres = getstr;
+  const edges = getres.data.edges;
+  expect(getres.success).toBeTruthy();
+  expect(edges.length).toBe(2);
+  expect(edges[0].node).toMatchObject(draft1);
+  expect(edges[1].node).toMatchObject(draft2);
+});
+
+test('Delete draft', async () => {
+  var result = await request(del(`/drafts/${draft1.id}`, userToken));
+  expect(result.success).toBeTruthy();
+  result = await request(del(`/drafts/${draft2.id}`, userToken));
   expect(result.success).toBeTruthy();
 });
 
-test('nothing', async () => {
-
+afterAll(async () => {
+  UsersRepo.deleteUserById(userId);
 });
