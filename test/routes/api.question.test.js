f25e3ea8ae74bdea2018a0939e033e88b82aa32b
Jehron Petty <jehronpetty@cornell.edu>
2018-05-09 00:24:06 -0400

    finished route tests

--- test/routes/api.question.test.js
+++ test/routes/api.question.test.js
@@ -36,6 +36,48 @@ test('create question', async () => {
   expect(result.success).toBeTruthy();
 });
 
+test('create question with invalid token', async () => {
+  const opts = {
+    text: 'Why do we have to test shit? (PG-13)'
+  };
+  const result = await request(post(`/sessions/${session.id}/questions`, opts, adminToken));
+  expect(result.success).toBeFalsy();
+});
+
+test('get question by id', async () => {
+  const getstr = await request(get(`/questions/${question.id}`, memberToken));
+  const getres = getstr;
+  expect(getres.success).toBeTruthy();
+  expect(question.id).toBe(getres.data.node.id);
+});
+
+test('get questions by session', async () => {
+  const getstr = await request(get(`/sessions/${session.id}/questions`, adminToken));
+  const getres = getstr;
+  expect(getres.success).toBeTruthy();
+  expect(question.id).toBe(getres.data.edges[0].node.id);
+});
+
+test('update question', async () => {
+  const opts = {
+    text: 'Why do we have to test stuff? (PG)'
+  };
+  const getstr = await request(put(`/questions/${question.id}`, opts, memberToken));
+  const getres = getstr;
+  expect(getres.success).toBeTruthy();
+  expect(getres.data.node.text).toBe('Why do we have to test stuff? (PG)');
+  expect(getres.data.node.id).toBe(question.id);
+});
+
+test('update question with invalid token', async () => {
+  const opts = {
+    text: 'Why do we have to test stuff? (PG)'
+  };
+  const getstr = await request(put(`/questions/${question.id}`, opts, adminToken));
+  const getres = getstr;
+  expect(getres.success).toBeFalsy();
+});
+
 test('delete question', async () => {
   const result = await request(del(`/questions/${question.id}`, memberToken));
   expect(result.success).toBeTruthy();
