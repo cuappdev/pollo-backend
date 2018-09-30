1b37e5ab954cd0f442381259ad24eb5a6f510f02
Megan Le <meganle7@gmail.com>
2018-09-30 14:16:27 -0400

    Add route for members to leave session

---
+++ src/routers/v2/Sessions/LeaveSessionRouter.js
@@ -0,0 +1,29 @@
+// @flow
+import { Request } from 'express';
+import AppDevRouter from '../../../utils/AppDevRouter';
+import constants from '../../../utils/constants';
+import SessionsRepo from '../../../repos/SessionsRepo';
+
+class LeaveSessionRouter extends AppDevRouter<Object> {
+    constructor() {
+        super(constants.REQUEST_TYPES.DELETE);
+    }
+
+    getPath(): string {
+        return '/sessions/:id/members/';
+    }
+
+    async content(req: Request) {
+        const sessionId = req.params.id;
+        const { user } = req;
+
+        if (await SessionsRepo.isAdmin(sessionId, user)) {
+            throw new Error('You are not allowed to leave your own session!');
+        }
+
+        await SessionsRepo.removeUserBySessionId(sessionId, user, 'member');
+        return null;
+    }
+}
+
+export default new LeaveSessionRouter().router;
