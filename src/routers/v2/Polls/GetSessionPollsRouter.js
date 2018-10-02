a291a3cdb20f84019aa0de238ef59c71d24a47b7
Megan Le <meganle7@gmail.com>
2018-10-02 14:49:55 -0400

    Standardize date

--- src/routers/v2/Polls/GetSessionPollsRouter.js
+++ src/routers/v2/Polls/GetSessionPollsRouter.js
@@ -22,9 +22,8 @@ class GetSessionPollsRouter extends AppDevRouter<Object> {
         // Date mapped to list of polls
         const pollsByDate = {};
         polls.filter(Boolean).forEach((poll) => {
-            let date = (new Date(1000 * poll.createdAt))
-                .toLocaleString('en-GB', { timeZone: 'UTC' });
-            date = date.substring(0, date.indexOf(','));
+            let date = (new Date(1000 * poll.createdAt)).toDateString();
+            date = date.substring(date.indexOf(' '));
             const p = {
                 id: poll.id,
                 text: poll.text,
