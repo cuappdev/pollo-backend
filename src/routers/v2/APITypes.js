14b2241263c7b131c2bb3015a5483461720f0f6c
Young Kim <young-k@protonmail.com>
2019-03-05 15:25:05 -0500

    Add updatedAt and isLive to JoinGroupRouter

--- src/routers/v2/APITypes.js
+++ src/routers/v2/APITypes.js
@@ -5,50 +5,52 @@
 export type id = string
 
 export type Response<T> = {
-  success: boolean,
   data: T,
-  errors?: Array<Error>
+  errors?: Array<Error>,
+  success: boolean,
 }
 
 // ************************ POLLO OBJECT TYPES ************************
 export type APIGroup = {|
   id: id,
+  code: string,
+  isLive: boolean,
   name: string,
-  code: string
+  updatedAt: string,
 |}
 
 export type APIPoll = {|
   id: id,
-  text: string,
+  answer: ?string,
+  correctAnswer: string,
   results: Object,
   shared: boolean,
+  text: string,
   type: string,
-  answer: ?string,
-  correctAnswer: string
 |}
 
 export type APIDraft = {|
   id: id,
-  text: string,
+  createdAt?: number,
   options: string[],
-  createdAt?: number
+  text: string,
 |}
 
 export type APIQuestion = {|
   id: id,
+  createdAt?: number,
   text: string,
-  createdAt?: number
 |}
 
 export type APIUser = {|
   id: id,
   name: string,
-  netID: string
+  netID: string,
 |}
 
 export type APIUserSession = {|
   accessToken: string,
+  isActive: boolean,
   refreshToken: string,
   sessionExpiration: number,
-  isActive: boolean
 |}
